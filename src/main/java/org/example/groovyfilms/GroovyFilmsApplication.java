5a50e65099a6a2b5c1258c6d790bd40dc2ec185e
João Antônio <joao.rodrigues@sankhya.com.br>
2026-02-23 08:32:18 -0300

    feat: First commit project

---
+++ src/main/java/org/example/groovyfilms/GroovyFilmsApplication.java
@@ -0,0 +1,13 @@
+package org.example.groovyfilms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovyFilmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroovyFilmsApplication.class, args);
+    }
+
+}
